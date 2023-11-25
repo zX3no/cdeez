0ec9d03a81e7673155960362b7d6e91b88508f41
zX3no <28522740+zX3no@users.noreply.github.com>
2023-11-25 19:57:14 +1030

    🧼

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@
 use std::{
     fs::File,
     io::{BufWriter, Write},
-    path::Path,
+    path::{Path, PathBuf},
+    str::from_utf8_unchecked,
 };
 
 #[derive(Debug)]
@@ -12,23 +13,17 @@ struct Location<'a> {
 }
 
 fn read_db(db: &str) -> Vec<Location> {
-    let lines = db.lines();
-    let mut locations = Vec::new();
-    for line in lines {
-        if line.is_empty() {
-            continue;
-        }
-
-        let end = line.find("\" ").unwrap();
-        unsafe {
-            let path = std::str::from_utf8_unchecked(&line.as_bytes()[1..end]);
-            let count = std::str::from_utf8_unchecked(&line.as_bytes()[end + 2..])
+    db.lines()
+        .filter(|line| !line.is_empty())
+        .map(|line| {
+            let end = line.find("\" ").unwrap();
+            let path = unsafe { from_utf8_unchecked(&line.as_bytes()[1..end]) };
+            let count = unsafe { from_utf8_unchecked(&line.as_bytes()[end + 2..]) }
                 .parse::<usize>()
                 .unwrap();
-            locations.push(Location { path, count });
-        }
-    }
-    locations
+            Location { path, count }
+        })
+        .collect()
 }
 
 fn write_config(path: &Path, db_path: &Path, mut locations: Vec<Location>) {
@@ -42,32 +37,23 @@ fn write_config(path: &Path, db_path: &Path, mut locations: Vec<Location>) {
             found = true;
         }
 
-        writer.write_all(b"\"").unwrap();
-        writer.write_all(location.path.as_bytes()).unwrap();
-        writer.write_all(b"\" ").unwrap();
-        writer
-            .write_all(location.count.to_string().as_bytes())
-            .unwrap();
-        writer.write_all(b"\n").unwrap();
+        writeln!(&mut writer, r#""{}" {}"#, location.path, location.count).unwrap()
     }
 
     if !found {
-        writer.write_all(b"\"").unwrap();
-        writer.write_all(path.to_str().unwrap().as_bytes()).unwrap();
-        writer.write_all(b"\" ").unwrap();
-        writer.write_all(b"1").unwrap();
-        writer.write_all(b"\n").unwrap();
+        writeln!(&mut writer, r#""{}" 1"#, path.display()).unwrap();
     }
 }
 
+#[cfg(target_os = "windows")]
 fn main() {
-    #[cfg(target_os = "windows")]
+    // #[cfg(target_os = "windows")]
     let db_path = Path::new(&std::env::var("APPDATA").unwrap()).join(Path::new("cdeez\\cdeez.db"));
 
-    #[cfg(not(target_os = "windows"))]
-    let path = Path::new(&std::env::var("HOME").unwrap())
-        .join(".config")
-        .join(Path::new("cdeez\\cdeez.db"));
+    // #[cfg(not(target_os = "windows"))]
+    // let path = Path::new(&std::env::var("HOME").unwrap())
+    //     .join(".config")
+    //     .join(Path::new("cdeez\\cdeez.db"));
 
     //Make sure the directory and database exists.
     let _ = std::fs::create_dir(db_path.parent().unwrap());
@@ -79,15 +65,12 @@ fn main() {
         return;
     }
 
-    match args[0].as_str() {
-        "--debug" => {
-            println!("cdeez: --debug");
-            for line in db.lines() {
-                println!("{line}");
-            }
-            std::process::exit(1);
+    if let "--list" = args[0].as_str() {
+        println!("cdeez: --list");
+        for line in db.lines() {
+            println!("{line}");
         }
-        _ => {}
+        return;
     }
 
     let pwd = std::env::current_dir().unwrap();
@@ -95,61 +78,80 @@ fn main() {
     let mut locations = read_db(&db);
 
     let path = match std::fs::canonicalize(&new) {
+        //Files cannot contain ':', the user must want a drive.
+        _ if args[0].ends_with(':') && args[0].len() == 2 => {
+            match std::fs::canonicalize(format!("{}\\", &args[0])) {
+                Ok(path) => path,
+                Err(_) => return println!("cdeez: cannot cd file '{}'", new.display()),
+            }
+        }
         Ok(path) if path.is_file() => {
             println!("cdeez: cannot cd file '{}'", new.display());
-            let locations: Vec<_> = locations
-                .extract_if(|location| Path::new(location.path) != path)
-                .collect();
-            write_config(&path, &db_path, locations);
-            std::process::exit(1);
+            locations.retain(|loc| Path::new(&loc.path) != path);
+            return write_config(&path, &db_path, locations);
         }
         //User wants to navigate to a directory in the current folder.
         Ok(path) => path,
         //User wants to go somewhere else.
         Err(_) => {
-            let (mut path, mut count) = (None, 0);
+            let mut path = None;
             let mut remove = false;
 
-            for l in &locations {
+            //TODO: Linux paths are case sensitive. 🙄
+            let user_input = &args[0].to_ascii_lowercase();
+            let normalized = user_input.replace('\\', "/");
+
+            let splits = if normalized.contains('/') {
+                let slashes: Vec<&str> = normalized.split('/').collect();
+                Some((slashes[0], slashes))
+            } else {
+                None
+            };
+
+            'outer: for l in locations.iter_mut() {
                 let lower = l.path.to_ascii_lowercase();
                 let target = Path::new(&lower);
-                if l.count > count && target.ends_with(&args[0].to_ascii_lowercase()) {
+
+                //Handle cases where 'foo' exists in the database but 'foo/bar' does not.
+                if let Some((root, splits)) = &splits {
+                    if target.ends_with(root) {
+                        let mut p = PathBuf::from(target);
+                        for split in splits {
+                            p = p.join(split);
+                            if !p.exists() {
+                                continue 'outer;
+                            }
+                        }
+
+                        path = Some(p);
+                        break;
+                    }
+                } else if target.ends_with(&normalized) {
                     if !target.exists() {
                         remove = true;
                     }
 
-                    path = Some(Path::new(l.path));
-                    count = l.count;
+                    path = Some(PathBuf::from(l.path));
+                    break;
                 }
             }
 
             let Some(path) = path else {
-                println!("cdeez: could not find '{}'", &args[0]);
-                std::process::exit(1);
+                return println!("cdeez: could not find '{}'", &args[0]);
             };
 
             //Path exists in database but not on file system.
             if remove {
                 println!("cdeez: removing dead path '{}'", &args[0]);
-                let mut locations: Vec<_> = locations
-                    .extract_if(|location| Path::new(location.path) != path)
-                    .collect();
+                locations.retain(|loc| Path::new(&loc.path) != path);
 
                 //Update the config removing the dead path.
-                let file = File::create(&db_path).unwrap();
+                let file = File::create(db_path).expect("Unable to create file");
                 let mut writer = BufWriter::new(file);
-                for location in locations.iter_mut() {
-                    writer.write_all(b"\"").unwrap();
-                    writer.write_all(location.path.as_bytes()).unwrap();
-                    writer.write_all(b"\" ").unwrap();
-                    writer
-                        .write_all(location.count.to_string().as_bytes())
-                        .unwrap();
-                    writer.write_all(b"\n").unwrap();
+                for location in locations {
+                    writeln!(&mut writer, r#""{}" {}"#, location.path, location.count).unwrap();
                 }
-
-                writer.flush().unwrap();
-                std::process::exit(1);
+                return;
             }
 
             path.to_path_buf()
@@ -157,8 +159,6 @@ fn main() {
     };
 
     write_config(&path, &db_path, locations);
-
-    //Output the path.
-    //TODO: Could this be made faster?
     println!("{}", path.display());
+    std::process::exit(1);
 }
